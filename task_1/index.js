37fa6c8ef9cd38f9ef22d22e8b6a041f79dcfe6f
Bishwajit Das <bishwajitkuat@gmail.com>
2023-01-17 14:47:49 +0200

    task_2

--- task_1/index.js
+++ task_1/index.js
@@ -1,3 +1,20 @@
-alert("Welcome to learning JavaScript!");
+// alert("Welcome to learning JavaScript!");
 const my_name = (name) => console.log(name);
 my_name("Bishwajit");
+
+const a = 3;
+const b = 4;
+// addition
+console.log(b + a);
+console.log(b % a);
+console.log(" I am string");
+console.log("I am number", 4);
+console.log(`Template leteral example ${5 * 5}`);
+console.log("Double \nline");
+console.log("Bishwajit" + " " + "Das");
+console.log(`I have ${2022 - 2008} years of experience`);
+console.log(5 > 2);
+console.log("Apple" == "apple");
+console.log("2" == 2);
+console.log(2 === +"2");
+console.log(2 === Number("2"));
