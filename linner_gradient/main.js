4df0974a8cbe8ed1f22414af4784ed31f29a924b
Bishwajit Das <bishwajitkuat@gmail.com>
2023-02-07 13:33:29 +0200

    linner gradient

--- linner_gradient/main.js
+++ linner_gradient/main.js
@@ -1,9 +1,20 @@
-document.querySelector("form").addEventListener("change", function () {
-  const first_color = document.querySelector("#first_color").value;
-  const second_color = document.querySelector("#second_color").value;
-  console.log(first_color, second_color);
+let first_color = document.querySelector("#first_color");
+let second_color = document.querySelector("#second_color");
+let degree = 0;
+let dir_id;
+
+document.querySelector("form").addEventListener("input", function (e) {
+  e.preventDefault();
+
+  if (e.target.type === "radio") {
+    degree = e.target.value;
+    dir_id = e.target.id;
+  }
 
   document.querySelector(
     "body"
-  ).style.background = `linear-gradient(${first_color}, ${second_color})`;
+  ).style.backgroundImage = `linear-gradient(${degree}deg ,${first_color.value}, ${second_color.value})`;
+  document.querySelector("#color1").textContent = `${first_color.value}  to `;
+  document.querySelector("#color2").textContent = second_color.value;
+  document.querySelector("#dir_star").textContent = `From ${dir_id}`;
 });
